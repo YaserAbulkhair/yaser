99031220707869f820949afc80f0d13835518cd7
YaserAbulkhair <96765334+YaserAbulkhair@users.noreply.github.com>
2021-12-29 14:37:06 +0700

    Add files via upload

---
+++ score.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+     int score = 50 , scor; 
+  printf ("enter your score :");
+  scanf("%d",&scor);
+  if (scor>=50)
+  printf ("you passed examination");
+  if (scor< score)
+  printf ("you fail examination");
+  
+
+    return 0;
+}
